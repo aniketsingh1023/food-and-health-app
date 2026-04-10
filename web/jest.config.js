54c7b3536ee21538e86f9b35adc682acdf86cbf9
aniketsingh1023 <aniketsinghn10@gmail.com>
2026-04-10 16:07:59 +0530

    fix: jest.config.ts → .js to avoid ts-node requirement in CI

--- web/jest.config.js
+++ web/jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-
-const config: Config = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
@@ -24,4 +23,4 @@ const config: Config = {
   ],
 };
 
-export default config;
+module.exports = config;
